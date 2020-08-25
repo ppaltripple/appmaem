31f354919444c117fbb154ba817407876ecd1696
ppaltripple <63242828+ppaltripple@users.noreply.github.com>
2020-08-25 16:59:23 +0300

    Updates

---
+++ precache-manifest.d8811faf98f3ffbd31968e29909afd91.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11bf6c95cbe12aafaa525e0a9c1da0ed",
+    "url": "./index.html"
+  },
+  {
+    "revision": "24b6fde1c9503c0a4199",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "f3d032f7e879607b3e56",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "24b6fde1c9503c0a4199",
+    "url": "./static/js/2.15bf3773.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.15bf3773.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3d032f7e879607b3e56",
+    "url": "./static/js/main.2fd43d14.chunk.js"
+  },
+  {
+    "revision": "f4753847c54287083994",
+    "url": "./static/js/runtime-main.1c2acb6c.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
